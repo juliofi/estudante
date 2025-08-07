c2c070fafc56fe9f2276120e41e42deb5ce95e27
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-08-06 22:01:47 -0300

    estudantes

---
+++ Estudantes.java
@@ -0,0 +1,17 @@
+public class Estudantes {
+    private String nome;
+    private int idade;
+
+    public Estudantes(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+}
